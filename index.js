941668b01280dc4e3ed634281c068ecd0b08701b
t1ngyuz <ztyfreya@gmail.com>
2023-09-27 16:19:42 +0900

    exercise 4.2

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://ztyfreya:zty2000@cluster0.4nco7g3.mongodb.net/bloglistApp?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.send("welcome")
+})
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
